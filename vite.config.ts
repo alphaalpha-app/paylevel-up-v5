a3007225188797dbcf4b524ae613117b48ea3cc6
alphaalpha-app <alphaleefs@gmail.com>
2025-11-24 14:22:01 +1030

    Fix base path: change from v4 to v5

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-            base: '/paylevel-up-v4/',
+            base: '/paylevel-up-vv5/',
       server: {
         port: 3000,
         host: '0.0.0.0',
